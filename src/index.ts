9ba2e1b3d07aafe084cde48bfda446de7623b51e
allison-truhlar <truhlara@janelia.hhmi.org>
2025-01-17 17:08:30 -0500

    feat: adds basic React widget
    Follows the JupyterLab React extension example

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,17 @@ import {
   JupyterFrontEnd,
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
+import { MainAreaWidget } from '@jupyterlab/apputils';
+import { ILauncher } from '@jupyterlab/launcher';
+import { reactIcon } from '@jupyterlab/ui-components';
+import { CounterWidget } from './widget';
+
+/**
+ * The command IDs used by the react-widget plugin.
+ */
+namespace CommandIDs {
+  export const createReactWidget = 'create-react-widget';
+}
 
 /**
  * Initialization data for the fileglancer-frontend-ext extension.
@@ -10,8 +21,30 @@ const plugin: JupyterFrontEndPlugin<void> = {
   id: 'fileglancer-frontend-ext:plugin',
   description: 'React-based frontend extension for the Fileglancer app.',
   autoStart: true,
-  activate: (app: JupyterFrontEnd) => {
+  optional: [ILauncher],
+  activate: (app: JupyterFrontEnd, launcher: ILauncher) => {
     console.log('JupyterLab extension fileglancer-frontend-ext is activated!');
+    const { commands } = app;
+    const command = CommandIDs.createReactWidget;
+
+    commands.addCommand(command, {
+      label: 'React Widget',
+      icon: reactIcon,
+      execute: () => {
+        console.log('Create React widget command executed');
+        const content = new CounterWidget();
+        const widget = new MainAreaWidget<CounterWidget>({ content });
+        widget.title.label = 'React Widget';
+        widget.title.icon = reactIcon;
+        app.shell.add(widget, 'main');
+      }
+    });
+
+    if (launcher) {
+      launcher.add({
+        command
+      });
+    }
   }
 };
 
