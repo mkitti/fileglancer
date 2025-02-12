81418f742b93141e2c0641858d8a9ba8e0e3b117
Konrad Rokicki <rokickik@janelia.hhmi.org>
2025-02-11 21:06:16 -0500

    test: added tests for patch

--- fileglancer_server/tests/test_handlers.py
+++ fileglancer_server/tests/test_handlers.py
@@ -23,3 +23,27 @@ async def test_get_files(jp_fetch):
     assert isinstance(payload, dict)
     assert "files" in payload
     assert isinstance(payload["files"], list)
+
+
+async def test_patch_files(jp_fetch):
+    
+    # Create an empty directory
+    response = await jp_fetch("fileglancer/files/newdir", method="POST", body=json.dumps({"type": "directory"}))
+    assert response.code == 201
+
+    # Create an empty file in the new directory
+    response = await jp_fetch("fileglancer/files/newdir/newfile.txt", method="POST", body=json.dumps({"type": "file"}))
+    assert response.code == 201
+
+    # Change the permissions of the new file
+    response = await jp_fetch("fileglancer/files/newdir/newfile.txt", method="PATCH", body=json.dumps({"permissions": "-rw-r--r--"}))
+    assert response.code == 204
+
+    # Move the file out of the directory
+    response = await jp_fetch("fileglancer/files/newdir/newfile.txt", method="PATCH", body=json.dumps({"path": "newfile.txt"}))
+    assert response.code == 204
+
+    # Remove the empty directory
+    response = await jp_fetch("fileglancer/files/newdir", method="DELETE")
+    assert response.code == 204
+
