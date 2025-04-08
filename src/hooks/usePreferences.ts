7a7b56e774e6f962c0531874634bb2081b4f4417
allison-truhlar <truhlara@janelia.hhmi.org>
2025-04-08 14:12:51 -0400

    feat(usePreferences): adds funcs to handle a favorites changes and update preferences; get prefs on first app load

--- src/hooks/usePreferences.ts
+++ src/hooks/usePreferences.ts
@@ -1,4 +1,7 @@
 import * as React from 'react';
+import { useCookies } from 'react-cookie';
+import { getAPIPathRoot, sendGetRequest, sendPutRequest } from '../utils';
+
 export default function useFileBrowser() {
   const [zoneFavorites, setZoneFavorites] = React.useState<string[]>([]);
   const [fileSharePathFavorites, setFileSharePathFavorites] = React.useState<
@@ -7,30 +10,73 @@ export default function useFileBrowser() {
   const [directoryFavorites, setDirectoryFavorites] = React.useState<string[]>(
     []
   );
+  const [cookies] = useCookies(['_xsrf']);
 
-  const handleFavoriteChange = (item: string, type: string) => {
-    switch (type) {
-      case 'zone':
-        setZoneFavorites(prev =>
-          prev.includes(item)
-            ? prev.filter(zone => zone !== item)
-            : [...prev, item]
-        );
-        break;
-      case 'fileSharePath':
-        setFileSharePathFavorites(prev =>
-          prev.includes(item)
-            ? prev.filter(path => path !== item)
-            : [...prev, item]
-        );
-        break;
-      case 'directory':
-        setDirectoryFavorites(prev =>
-          prev.includes(item)
-            ? prev.filter(dir => dir !== item)
-            : [...prev, item]
-        );
-        break;
+  React.useEffect(() => {
+    const fetchPreferences = async () => {
+      try {
+        await sendGetRequest(
+          `${getAPIPathRoot()}api/fileglancer/preference?key=zoneFavorites`,
+          cookies['_xsrf']
+        )
+          .then(response => response.json())
+          .then(data => {
+            setZoneFavorites(data);
+          });
+        await sendGetRequest(
+          `${getAPIPathRoot()}api/fileglancer/preference?key=fileSharePathFavorites`,
+          cookies['_xsrf']
+        )
+          .then(response => response.json())
+          .then(data => {
+            setFileSharePathFavorites(data);
+          });
+        await sendGetRequest(
+          `${getAPIPathRoot()}api/fileglancer/preference?key=directoryFavorites`,
+          cookies['_xsrf']
+        )
+          .then(response => response.json())
+          .then(data => {
+            setDirectoryFavorites(data);
+          });
+      } catch (error) {
+        console.error('Error fetching preferences:', error);
+      }
+    };
+    fetchPreferences();
+  }, []);
+
+  const updatePreferences = async (key: string, body: string[]) => {
+    try {
+      await sendPutRequest(
+        `${getAPIPathRoot()}api/fileglancer/preference?key=${key}`,
+        cookies['_xsrf'],
+        body
+      );
+    } catch (error) {
+      console.error(`Error updating ${key}:`, error);
+    }
+  };
+
+  const handleFavoriteChange = async (item: string, type: string) => {
+    if (type === 'zone') {
+      const newFavorites = zoneFavorites.includes(item)
+        ? zoneFavorites.filter(zone => zone !== item)
+        : [...zoneFavorites, item];
+      setZoneFavorites(newFavorites);
+      updatePreferences('zoneFavorites', newFavorites);
+    } else if (type === 'fileSharePath') {
+      const newFavorites = fileSharePathFavorites.includes(item)
+        ? fileSharePathFavorites.filter(path => path !== item)
+        : [...fileSharePathFavorites, item];
+      setFileSharePathFavorites(newFavorites);
+      updatePreferences('fileSharePathFavorites', newFavorites);
+    } else if (type === 'directory') {
+      const newFavorites = directoryFavorites.includes(item)
+        ? directoryFavorites.filter(dir => dir !== item)
+        : [...directoryFavorites, item];
+      setDirectoryFavorites(newFavorites);
+      updatePreferences('directoryFavorites', newFavorites);
     }
   };
 
