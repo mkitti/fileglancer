1998e39d83d1f3cfa1acc6c9a01125848e0828b5
allison-truhlar <truhlara@janelia.hhmi.org>
2025-04-24 15:48:02 -0400

    fix(favorites): remove zone/file share/dir headers from favorites browser

--- src/components/ui/Sidebar/FavoritesBrowser.tsx
+++ src/components/ui/Sidebar/FavoritesBrowser.tsx
@@ -80,162 +80,66 @@ export default function FavoritesBrowser({
       </List>
       <div className="overflow-y-auto">
         <Collapse open={openFavorites['all'] ? true : false}>
-          <List className="bg-background pr-2">
-            <List.Item
-              onClick={() => toggleOpenFavorites('zones')}
-              className="cursor-pointer rounded-none "
-            >
-              <List.ItemStart>
-                <Squares2X2Icon className="h-4 w-4 !text-foreground" />
-              </List.ItemStart>
-              <Typography className="text-sm !text-foreground">
-                Zones
-              </Typography>
-              <List.ItemEnd>
-                <ChevronRightIcon
-                  className={`h-4 w-4 ${openFavorites['zones'] ? 'rotate-90' : ''}`}
+          <List className="bg-surface-light !py-0 !gap-0">
+            {/* Zone favorites */}
+            {displayZones.map(zoneWithPaths => {
+              const zoneName = Object.keys(zoneWithPaths)[0];
+              const fileSharePaths = zoneWithPaths[zoneName];
+              return (
+                <Zone
+                  zoneName={zoneName}
+                  fileSharePaths={fileSharePaths}
+                  openZones={openFavorites}
+                  toggleOpenZones={toggleOpenFavorites}
                 />
-              </List.ItemEnd>
-            </List.Item>
-            <Collapse open={openFavorites['zones'] ? true : false}>
-              <List className="bg-surface-light !py-0 !gap-0">
-                {displayZones.map(zoneWithPaths => {
-                  const zoneName = Object.keys(zoneWithPaths)[0];
-                  const fileSharePaths = zoneWithPaths[zoneName];
-                  return (
-                    <Zone
-                      zoneName={zoneName}
-                      fileSharePaths={fileSharePaths}
-                      openZones={openFavorites}
-                      toggleOpenZones={toggleOpenFavorites}
-                    />
-                  );
-                })}
-              </List>
-            </Collapse>
-          </List>
-        </Collapse>
-        <Collapse
-          open={openFavorites['all'] ? true : false}
-          className="max-h-[calc(100vh-250px)]"
-        >
-          <List className="bg-background pr-2">
-            <List.Item
-              onClick={() => toggleOpenFavorites('fileSharePaths')}
-              className="cursor-pointer rounded-none"
-            >
-              <List.ItemStart>
-                <RectangleStackIcon className="h-4 w-4 !text-foreground" />
-              </List.ItemStart>
-              <Typography className="text-sm !text-foreground">
-                File Share Paths
-              </Typography>
-              <List.ItemEnd>
-                <ChevronRightIcon
-                  className={`h-4 w-4 ${openFavorites['fileSharePaths'] ? 'rotate-90' : ''}`}
-                />
-              </List.ItemEnd>
-            </List.Item>
-            <Collapse open={openFavorites['fileSharePaths'] ? true : false}>
-              <List className="bg-surface-light !py-0 !gap-0">
-                {displayFileSharePaths.map((path, index) => {
-                  return <FileSharePath pathItem={path} pathIndex={index} />;
-                })}
-              </List>
-            </Collapse>
-          </List>
-        </Collapse>
-        <Collapse open={openFavorites['all'] ? true : false}>
-          <List className="bg-background pr-2">
-            <List.Item
-              onClick={() => toggleOpenFavorites('directories')}
-              className="cursor-pointer rounded-none"
-            >
-              <List.ItemStart>
-                <FolderIcon className="h-4 w-4 !text-foreground" />
-              </List.ItemStart>
-              <Typography className="text-sm !text-foreground">
-                Directories
-              </Typography>
-              <List.ItemEnd>
-                <ChevronRightIcon
-                  className={`h-4 w-4 ${openFavorites['directories'] ? 'rotate-90' : ''}`}
-                />
-              </List.ItemEnd>
-            </List.Item>
-            <Collapse open={openFavorites['directories'] ? true : false}>
-              <List className="bg-surface-light max-h-[calc(40vh)] !py-0 !gap-0">
-                {displayDirectories.map(directoryItem => {
-                  return (
-                    <List.Item
-                      key={`favorite-directory-${directoryItem.name}`}
-                      onClick={() => {
-                        setOpenZones({
-                          all: true,
-                          [directoryItem.navigationZone]: true
-                        });
-                        setCurrentNavigationZone(directoryItem.navigationZone);
-                        setCurrentFileSharePath(directoryItem.fileSharePath);
-                        fetchAndFormatFilesForDisplay(
-                          `${directoryItem.fileSharePath}?subpath=${directoryItem.path}`
-                        );
-                      }}
-                      className="flex gap-2 items-center justify-between pl-5 rounded-none cursor-pointer text-foreground hover:bg-primary-light/30 focus:bg-primary-light/30 !bg-background"
-                    >
-                      <Link
-                        to="/files"
-                        className="flex flex-col gap-2 !text-foreground hover:!text-black focus:!text-black hover:dark:!text-white focus:dark:!text-white"
-                      >
-                        <div className="flex gap-1 items-center">
-                          {/* <FolderIcon className="h-4 w-4" /> */}
-                          <Typography className="text-sm font-medium leading-4">
-                            {directoryItem.name}
-                          </Typography>
-                        </div>
-                        <Typography className="text-xs">
-                          {directoryItem.path}
-                        </Typography>
-                      </Link>
-                    </List.Item>
-                  );
-                })}
-              </List>
-            </Collapse>
+              );
+            })}
+
+            {/* File share path favorites */}
+            {displayFileSharePaths.map((path, index) => {
+              return <FileSharePath pathItem={path} pathIndex={index} />;
+            })}
+
+            {/* Directory favorites */}
+            {displayDirectories.map(directoryItem => {
+              console.log('current file share path:', currentFileSharePath);
+              console.log('current dir:', currentDir);
+              return (
+                <List.Item
+                  key={`favorite-directory-${directoryItem.name}`}
+                  onClick={() => {
+                    setOpenZones({
+                      all: true,
+                      [directoryItem.navigationZone]: true
+                    });
+                    setCurrentNavigationZone(directoryItem.navigationZone);
+                    setCurrentFileSharePath(directoryItem.fileSharePath);
+                    fetchAndFormatFilesForDisplay(
+                      `${directoryItem.fileSharePath}?subpath=${directoryItem.path}`
+                    );
+                  }}
+                  className={`flex gap-2 items-center justify-between rounded-none cursor-pointer text-foreground hover:bg-primary-light/30 focus:bg-primary-light/30 ${directoryItem.fileSharePath === currentFileSharePath && directoryItem.name === currentDir ? '!bg-primary-light/30' : '!bg-background'}`}
+                >
+                  <Link
+                    to="/files"
+                    className="flex flex-col gap-2 !text-foreground hover:!text-black focus:!text-black hover:dark:!text-white focus:dark:!text-white"
+                  >
+                    <div className="flex gap-1 items-center">
+                      <FolderIcon className="h-4 w-4" />
+                      <Typography className="text-sm font-medium leading-4">
+                        {directoryItem.name}
+                      </Typography>
+                    </div>
+                    <Typography className="text-xs">
+                      {directoryItem.path}
+                    </Typography>
+                  </Link>
+                </List.Item>
+              );
+            })}
           </List>
         </Collapse>
       </div>
     </div>
   );
 }
-
-// <List.Item
-//   key={`favorite-fileSharePath-${path}`}
-//   onClick={() => {
-//     setOpenZones({ all: true, [path.zone]: true });
-//     setCurrentNavigationZone(path.zone);
-//     setCurrentFileSharePath(path.name);
-//     fetchAndFormatFilesForDisplay(path.name);
-//   }}
-//   className={`flex gap-2 items-center justify-between pl-5 rounded-none cursor-pointer text-foreground hover:bg-primary-light/30 focus:bg-primary-light/30 ${isCurrentPath ? '!bg-primary-light/30' : '!bg-background'}`}
-// >
-//   <Link
-//     to="/files"
-//     className="flex flex-col gap-2 !text-foreground hover:!text-black focus:!text-black hover:dark:!text-white focus:dark:!text-white"
-//   >
-//     <div className="flex gap-1 items-center">
-//       {/* <FolderIcon className="h-4 w-4" /> */}
-//       <Typography className="text-sm font-medium leading-4">
-//         {path.storage}
-//       </Typography>
-//     </div>
-//     <Typography className="text-xs">
-//       {pathPreference[0] === 'linux_path'
-//         ? path.linux_path
-//         : pathPreference[0] === 'windows_path'
-//           ? path.windows_path
-//           : pathPreference[0] === 'mac_path'
-//             ? path.mac_path
-//             : path.linux_path}
-//     </Typography>
-//   </Link>
-// </List.Item>
