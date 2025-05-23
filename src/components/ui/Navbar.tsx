1b32a13d2dabd6cc46ab450d882610c23aa44459
allison-truhlar <truhlara@janelia.hhmi.org>
2025-05-23 17:15:46 -0400

    wip: starts adding sharing page

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -14,6 +14,7 @@ import {
   Bars3Icon as MenuIcon,
   BriefcaseIcon,
   XMarkIcon,
+  ShareIcon,
   SunIcon
 } from '@heroicons/react/24/outline';
 import ProfileMenu from './Navbar/ProfileMenu';
@@ -25,6 +26,11 @@ const LINKS = [
     icon: FolderIcon,
     title: 'Browse',
     href: '/browse'
+  },
+  {
+    icon: ShareIcon,
+    title: 'Sharing',
+    href: '/sharing'
   },
   {
     icon: BriefcaseIcon,
