9095bb65e84c19221d5ae5723a7ee4577f90a1bd
Konrad Rokicki <rokickik@janelia.hhmi.org>
2025-05-25 16:46:33 -0400

    fixed layout

--- src/components/ui/FileBrowser/ZarrPreview.tsx
+++ src/components/ui/FileBrowser/ZarrPreview.tsx
@@ -18,6 +18,7 @@ import { useProxiedPathContext } from '@/contexts/ProxiedPathContext';
 import { useFileBrowserContext } from '@/contexts/FileBrowserContext';
 import ZarrMetadataTable from './ZarrMetadataTable';
 import SharingDialog from '../Dialogs/SharingDialog';
+import { Divide } from 'iconoir-react';
 
 type ZarrPreviewProps = {
   thumbnailSrc: string | null;
@@ -49,129 +50,133 @@ export default function ZarrPreview({
   }, [proxiedPath]);
 
   return (
-    <div className="flex gap-12 my-4 p-4 bg-primary-light/30 shadow-sm rounded-md w-full h-fit max-h-100">
-      <div className="flex flex-col gap-4">
-        <div className="flex flex-col gap-2 max-h-full">
-          <Typography variant="small" className="text-surface-foreground">
-            {loadingThumbnail ? 'Loading OME-Zarr image thumbnail...' : ''}
-          </Typography>
-          {loadingThumbnail ? (
-            <div
-              className="w-10 h-10 border-4 border-surface-foreground border-t-transparent rounded-full animate-spin"
-              title="Loading Thumbnail..."
-            ></div>
-          ) : null}
-          {!loadingThumbnail && thumbnailSrc ? (
-            <img
-              id="thumbnail"
-              src={thumbnailSrc}
-              alt="Thumbnail"
-              className="max-h-72 max-w-max rounded-md"
-            />
-          ) : null}
-        </div>
-
-        {neuroglancerUrl && !isImageShared ? (
-          <div>
-            <Typography className="text-sm font-semibold text-surface-foreground">
-              To view this image in external viewers like Neuroglancer, please
-              share the image first.
+    <div className="my-4 p-4 shadow-sm rounded-md bg-primary-light/30">
+      <div className="flex gap-12 w-full h-fit max-h-100">
+        <div className="flex flex-col gap-4">
+          <div className="flex flex-col gap-2 max-h-full">
+            <Typography variant="small" className="text-surface-foreground">
+              {loadingThumbnail ? 'Loading OME-Zarr image thumbnail...' : ''}
             </Typography>
-            <Button
-              className="mt-2 bg-secondary-light border-secondary-light hover:!bg-secondary-light/80 hover:!border-secondary-light/80"
-              onClick={() => {
-                setShowSharingDialog(true);
-              }}
-            >
-              Share Image
-            </Button>
-          </div>
-        ) : null}
-
-        {showSharingDialog ? (
-          <SharingDialog
-            filePath={filePath}
-            filePathWithoutFsp={filePathWithoutFsp}
-            showSharingDialog={showSharingDialog}
-            setShowSharingDialog={setShowSharingDialog}
-          />
-        ) : null}
-
-        {neuroglancerUrl && isImageShared ? (
-          <div>
-            <Typography className="font-semibold text-sm text-surface-foreground">
-              Open with:
-            </Typography>
-            <ButtonGroup className="relative">
-              <Button
-                as={Link}
-                to={neuroglancerUrl}
-                title="View in Neuroglancer"
-                target="_blank"
-                rel="noopener noreferrer"
-                variant="ghost"
-                className="rounded-sm m-0 p-0"
-              >
-                <img
-                  src={neuroglancer_logo}
-                  alt="Neuroglancer logo"
-                  className="max-h-8 max-w-8 m-1 rounded-sm"
-                />
-              </Button>
-              <Button
-                as={Link}
-                to="#"
-                title="View in Vol-E"
-                target="_blank"
-                rel="noopener noreferrer"
-                variant="ghost"
-                className="rounded-sm m-0 p-0"
-              >
-                <img
-                  src={volE_logo}
-                  alt="Vol-E logo"
-                  className="max-h-8 max-w-8 m-1 rounded-sm"
-                />
-              </Button>
-              <div>
-                <Button
-                  title="Copy link to view in Napari"
-                  variant="ghost"
-                  className="group peer/napari rounded-sm m-0 p-0 relative"
-                  onClick={() => {
-                    copyToClipboard('Napari URL');
-                  }}
-                >
-                  <img
-                    src={napari_logo}
-                    alt="Napari logo"
-                    className="max-h-8 max-w-8 m-1 rounded-sm"
-                  />
-                  <Square2StackIcon className="w-4 h-4 text-transparent group-hover:text-foreground absolute top-0 right-0 bg-transparent group-hover:bg-background" />
-                </Button>
-                <Typography
-                  className={`!hidden text-transparent
-                  ${showCopyAlert !== true && 'peer-hover/napari:text-foreground peer-hover/napari:bg-background peer-hover/napari:!block'}
-                  absolute top-12 left-0 bg-transparent w-fit px-1 rounded-sm`}
-                >
-                  See <a href="https://napari.org">napari.org</a> for
-                  instructions. Then <code>napari URL</code>
-                </Typography>
-              </div>
-            </ButtonGroup>
-            {showCopyAlert === true ? (
-              <Alert className="flex items-center max-w-max p-1 bg-secondary-light/70 border-none">
-                <Alert.Content>URL copied to clipboard!</Alert.Content>
-                <XMarkIcon
-                  className="h-5 w-5 cursor-pointer"
-                  onClick={dismissCopyAlert}
-                />
-              </Alert>
+            {loadingThumbnail ? (
+              <div
+                className="w-10 h-10 border-4 border-surface-foreground border-t-transparent rounded-full animate-spin"
+                title="Loading Thumbnail..."
+              ></div>
+            ) : null}
+            {!loadingThumbnail && thumbnailSrc ? (
+              <img
+                id="thumbnail"
+                src={thumbnailSrc}
+                alt="Thumbnail"
+                className="max-h-72 max-w-max rounded-md"
+              />
             ) : null}
           </div>
-        ) : null}
+
+          {neuroglancerUrl && !isImageShared ? (
+            <div>
+              <Button
+                className="mt-2 bg-secondary-light border-secondary-light hover:!bg-secondary-light/80 hover:!border-secondary-light/80"
+                onClick={() => {
+                  setShowSharingDialog(true);
+                }}
+              >
+                Share Image
+              </Button>
+            </div>
+          ) : null}
+
+          {showSharingDialog ? (
+            <SharingDialog
+              filePath={filePath}
+              filePathWithoutFsp={filePathWithoutFsp}
+              showSharingDialog={showSharingDialog}
+              setShowSharingDialog={setShowSharingDialog}
+            />
+          ) : null}
+
+          {neuroglancerUrl && isImageShared ? (
+            <div>
+              <Typography className="font-semibold text-sm text-surface-foreground">
+                Open with:
+              </Typography>
+              <ButtonGroup className="relative">
+                <Button
+                  as={Link}
+                  to={neuroglancerUrl}
+                  title="View in Neuroglancer"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  variant="ghost"
+                  className="rounded-sm m-0 p-0"
+                >
+                  <img
+                    src={neuroglancer_logo}
+                    alt="Neuroglancer logo"
+                    className="max-h-8 max-w-8 m-1 rounded-sm"
+                  />
+                </Button>
+                <Button
+                  as={Link}
+                  to="#"
+                  title="View in Vol-E"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  variant="ghost"
+                  className="rounded-sm m-0 p-0"
+                >
+                  <img
+                    src={volE_logo}
+                    alt="Vol-E logo"
+                    className="max-h-8 max-w-8 m-1 rounded-sm"
+                  />
+                </Button>
+                <div>
+                  <Button
+                    title="Copy link to view in Napari"
+                    variant="ghost"
+                    className="group peer/napari rounded-sm m-0 p-0 relative"
+                    onClick={() => {
+                      copyToClipboard('Napari URL');
+                    }}
+                  >
+                    <img
+                      src={napari_logo}
+                      alt="Napari logo"
+                      className="max-h-8 max-w-8 m-1 rounded-sm"
+                    />
+                    <Square2StackIcon className="w-4 h-4 text-transparent group-hover:text-foreground absolute top-0 right-0 bg-transparent group-hover:bg-background" />
+                  </Button>
+                  <Typography
+                    className={`!hidden text-transparent
+                    ${showCopyAlert !== true && 'peer-hover/napari:text-foreground peer-hover/napari:bg-background peer-hover/napari:!block'}
+                    absolute top-12 left-0 bg-transparent w-fit px-1 rounded-sm`}
+                  >
+                    See <a href="https://napari.org">napari.org</a> for
+                    instructions. Then <code>napari URL</code>
+                  </Typography>
+                </div>
+              </ButtonGroup>
+              {showCopyAlert === true ? (
+                <Alert className="flex items-center max-w-max p-1 bg-secondary-light/70 border-none">
+                  <Alert.Content>URL copied to clipboard!</Alert.Content>
+                  <XMarkIcon
+                    className="h-5 w-5 cursor-pointer"
+                    onClick={dismissCopyAlert}
+                  />
+                </Alert>
+              ) : null}
+            </div>
+          ) : null}
+        </div>
+        {metadata && <ZarrMetadataTable metadata={metadata} />}
       </div>
-      {metadata && <ZarrMetadataTable metadata={metadata} />}
+      {!isImageShared ? (
+      <Typography className="text-sm font-semibold text-surface-foreground">
+        To view this image in external viewers like Neuroglancer, please
+        share the image first.
+      </Typography>
+      ) : null}
     </div>
   );
 }
