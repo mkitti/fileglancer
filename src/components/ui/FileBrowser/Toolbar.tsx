b0ca911c24a8a976eef618c8090fe82dcd549bd9
Konrad Rokicki <rokickik@janelia.hhmi.org>
2025-05-25 17:33:41 -0400

    reorder toolbar

--- src/components/ui/FileBrowser/Toolbar.tsx
+++ src/components/ui/FileBrowser/Toolbar.tsx
@@ -32,21 +32,18 @@ export default function Toolbar({
   return (
     <div className="flex flex-col min-w-full p-2 border-b border-surface">
       <ButtonGroup className="self-start">
-        {/* Show/hide dot files and folders */}
+
+        {/* Refresh browser contents */}
         <Tooltip placement="top">
           <Tooltip.Trigger
             as={IconButton}
             variant="outline"
-            onClick={() => setHideDotFiles((prev: boolean) => !prev)}
+            onClick={() => fetchAndFormatFilesForDisplay(currentNavigationPath)}
           >
-            {hideDotFiles ? (
-              <EyeSlashIcon className="icon-default" />
-            ) : (
-              <EyeIcon className="icon-default" />
-            )}
+            <ArrowPathIcon className="icon-default" />
             <Tooltip.Content className="px-2.5 py-1.5 text-primary-foreground">
               <Typography type="small" className="opacity-90">
-                {hideDotFiles ? 'Show dot files' : 'Hide dot files'}
+                Refresh file browser
               </Typography>
               <Tooltip.Arrow />
             </Tooltip.Content>
@@ -72,6 +69,27 @@ export default function Toolbar({
           </Tooltip.Content>
         </Tooltip>
 
+        {/* Show/hide dot files and folders */}
+        <Tooltip placement="top">
+          <Tooltip.Trigger
+            as={IconButton}
+            variant="outline"
+            onClick={() => setHideDotFiles((prev: boolean) => !prev)}
+          >
+            {hideDotFiles ? (
+              <EyeSlashIcon className="icon-default" />
+            ) : (
+              <EyeIcon className="icon-default" />
+            )}
+            <Tooltip.Content className="px-2.5 py-1.5 text-primary-foreground">
+              <Typography type="small" className="opacity-90">
+                {hideDotFiles ? 'Show dot files' : 'Hide dot files'}
+              </Typography>
+              <Tooltip.Arrow />
+            </Tooltip.Content>
+          </Tooltip.Trigger>
+        </Tooltip>
+
         {/* Show/hide properties drawer */}
         <Tooltip placement="top">
           <Tooltip.Trigger
@@ -89,22 +107,6 @@ export default function Toolbar({
           </Tooltip.Trigger>
         </Tooltip>
 
-        {/* Refresh browser contents */}
-        <Tooltip placement="top">
-          <Tooltip.Trigger
-            as={IconButton}
-            variant="outline"
-            onClick={() => fetchAndFormatFilesForDisplay(currentNavigationPath)}
-          >
-            <ArrowPathIcon className="icon-default" />
-            <Tooltip.Content className="px-2.5 py-1.5 text-primary-foreground">
-              <Typography type="small" className="opacity-90">
-                Refresh file browser
-              </Typography>
-              <Tooltip.Arrow />
-            </Tooltip.Content>
-          </Tooltip.Trigger>
-        </Tooltip>
       </ButtonGroup>
     </div>
   );
