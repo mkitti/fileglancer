efb4b3147ec7b079e676c09ee06a45d13c29c7fa
Cristian Goina <goinac@janelia.hhmi.org>
2025-06-07 10:25:12 -0400

    run all ui-tests with reuse server false

--- ui-tests/playwright.config.js
+++ ui-tests/playwright.config.js
@@ -13,7 +13,7 @@ module.exports = {
   webServer: {
     command: 'npm start',
     url: 'http://localhost:8888/lab',
-    timeout: 120 * 1000,
-    reuseExistingServer: true,
+    timeout: 20 * 1000,
+    reuseExistingServer: false,
   }
 };
