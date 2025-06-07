6b71b973c19c5096f9dbe14089b9caac77c7429b
Cristian Goina <goinac@janelia.hhmi.org>
2025-06-07 10:17:50 -0400

    try again reuse server

--- ui-tests/playwright.config.js
+++ ui-tests/playwright.config.js
@@ -14,6 +14,6 @@ module.exports = {
     command: 'npm start',
     url: 'http://localhost:8888/lab',
     timeout: 120 * 1000,
-    reuseExistingServer: false,
+    reuseExistingServer: true,
   }
 };
