01cd9fb08ebe1f5382673abacd1f63e699984d75
allison-truhlar <truhlara@janelia.hhmi.org>
2025-06-26 13:12:56 -0400

    feat: implement OpenFavorites context and provider
    - Create OpenFavoritesContext for managing open favorites state
    - Update handleFavoriteClick in PreferencesProvider to utilize openFavoritesSection
    - Remove old useToggleOpenFavorites hook

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -6,29 +6,32 @@ import { ZonesAndFspMapContextProvider } from '@/contexts/ZonesAndFspMapContext'
 import { FileBrowserContextProvider } from '@/contexts/FileBrowserContext';
 import { PreferencesProvider } from '@/contexts/PreferencesContext';
 import { ProxiedPathProvider } from '@/contexts/ProxiedPathContext';
+import { OpenFavoritesProvider } from '@/contexts/OpenFavoritesContext';
 import FileglancerNavbar from '@/components/ui/Navbar';
 
 export const MainLayout = () => {
   return (
     <CookiesProvider>
       <ZonesAndFspMapContextProvider>
-        <PreferencesProvider>
-          <FileBrowserContextProvider>
-            <ProxiedPathProvider>
-              <Toaster
-                position="bottom-center"
-                toastOptions={{
-                  className: 'min-w-fit',
-                  success: { duration: 4000 }
-                }}
-              />
-              <div className="flex flex-col items-center h-full w-full overflow-y-hidden bg-background text-foreground box-border">
-                <FileglancerNavbar />
-                <Outlet />
-              </div>
-            </ProxiedPathProvider>
-          </FileBrowserContextProvider>
-        </PreferencesProvider>
+        <OpenFavoritesProvider>
+          <PreferencesProvider>
+            <FileBrowserContextProvider>
+              <ProxiedPathProvider>
+                <Toaster
+                  position="bottom-center"
+                  toastOptions={{
+                    className: 'min-w-fit',
+                    success: { duration: 4000 }
+                  }}
+                />
+                <div className="flex flex-col items-center h-full w-full overflow-y-hidden bg-background text-foreground box-border">
+                  <FileglancerNavbar />
+                  <Outlet />
+                </div>
+              </ProxiedPathProvider>
+            </FileBrowserContextProvider>
+          </PreferencesProvider>
+        </OpenFavoritesProvider>
       </ZonesAndFspMapContextProvider>
     </CookiesProvider>
   );
