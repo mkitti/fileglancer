ef202ec83052acf1c3b6f4bd277fea9e8f262b01
allison-truhlar <truhlara@janelia.hhmi.org>
2025-07-15 10:29:06 -0400

    fix: redirect index route to browse

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router';
 import { CookiesProvider } from 'react-cookie';
 import { ErrorBoundary } from 'react-error-boundary';
 
