83f2cf6f9d79ce421f9c5da1277aa90484f27fce
allison-truhlar <truhlara@janelia.hhmi.org>
2025-07-18 17:00:42 -0400

    chore: remove duplicate error boundaries
    these are adjacent to the error boundaries in the main layout

--- src/layouts/BrowseLayout.tsx
+++ src/layouts/BrowseLayout.tsx
@@ -2,12 +2,10 @@ import React from 'react';
 import { Outlet } from 'react-router';
 import { Panel, PanelGroup, PanelResizeHandle } from 'react-resizable-panels';
 import { PiDotsSixVerticalBold } from 'react-icons/pi';
-import { ErrorBoundary } from 'react-error-boundary';
 
 import useShowPropertiesDrawer from '@/hooks/useShowPropertiesDrawer';
 import Sidebar from '@/components/ui/Sidebar/Sidebar';
 import PropertiesDrawer from '@/components/ui/PropertiesDrawer/PropertiesDrawer';
-import ErrorFallback from '@/components/ErrorFallback';
 
 export const BrowsePageLayout = () => {
   const [showPermissionsDialog, setShowPermissionsDialog] =
@@ -31,50 +29,48 @@ export const BrowsePageLayout = () => {
   };
 
   return (
-    <ErrorBoundary FallbackComponent={ErrorFallback}>
-      <div className="flex h-full w-full overflow-y-hidden">
-        <PanelGroup autoSaveId="conditional" direction="horizontal">
-          {showSidebar ? (
-            <>
-              <Panel
-                id="sidebar"
-                order={1}
-                defaultSize={18}
-                minSize={10}
-                maxSize={50}
-              >
-                <Sidebar />
-              </Panel>
-              <PanelResizeHandle className="group relative border-r border-surface hover:border-secondary/60">
-                <PiDotsSixVerticalBold className="icon-default stroke-2 absolute -right-1 top-1/2 stroke-black dark:stroke-white" />
-              </PanelResizeHandle>
-            </>
-          ) : null}
-          <Panel id="main" order={2}>
-            <Outlet context={outletContextValue} />
-          </Panel>
-          {showPropertiesDrawer ? (
-            <>
-              <PanelResizeHandle className="group relative w-3 bg-surface border-l border-surface hover:border-secondary/60">
-                <PiDotsSixVerticalBold className="icon-default stroke-2 absolute -left-1 top-1/2 stroke-black dark:stroke-white" />
-              </PanelResizeHandle>
-              <Panel
-                id="properties"
-                order={3}
-                defaultSize={18}
-                minSize={15}
-                maxSize={50}
-              >
-                <PropertiesDrawer
-                  setShowPropertiesDrawer={setShowPropertiesDrawer}
-                  setShowPermissionsDialog={setShowPermissionsDialog}
-                  setShowConvertFileDialog={setShowConvertFileDialog}
-                />
-              </Panel>
-            </>
-          ) : null}
-        </PanelGroup>
-      </div>
-    </ErrorBoundary>
+    <div className="flex h-full w-full overflow-y-hidden">
+      <PanelGroup autoSaveId="conditional" direction="horizontal">
+        {showSidebar ? (
+          <>
+            <Panel
+              id="sidebar"
+              order={1}
+              defaultSize={18}
+              minSize={10}
+              maxSize={50}
+            >
+              <Sidebar />
+            </Panel>
+            <PanelResizeHandle className="group relative border-r border-surface hover:border-secondary/60">
+              <PiDotsSixVerticalBold className="icon-default stroke-2 absolute -right-1 top-1/2 stroke-black dark:stroke-white" />
+            </PanelResizeHandle>
+          </>
+        ) : null}
+        <Panel id="main" order={2}>
+          <Outlet context={outletContextValue} />
+        </Panel>
+        {showPropertiesDrawer ? (
+          <>
+            <PanelResizeHandle className="group relative w-3 bg-surface border-l border-surface hover:border-secondary/60">
+              <PiDotsSixVerticalBold className="icon-default stroke-2 absolute -left-1 top-1/2 stroke-black dark:stroke-white" />
+            </PanelResizeHandle>
+            <Panel
+              id="properties"
+              order={3}
+              defaultSize={18}
+              minSize={15}
+              maxSize={50}
+            >
+              <PropertiesDrawer
+                setShowPropertiesDrawer={setShowPropertiesDrawer}
+                setShowPermissionsDialog={setShowPermissionsDialog}
+                setShowConvertFileDialog={setShowConvertFileDialog}
+              />
+            </Panel>
+          </>
+        ) : null}
+      </PanelGroup>
+    </div>
   );
 };
--- src/layouts/OtherPagesLayout.tsx
+++ src/layouts/OtherPagesLayout.tsx
@@ -1,14 +1,9 @@
 import { Outlet } from 'react-router';
-import { ErrorBoundary } from 'react-error-boundary';
-
-import ErrorFallback from '@/components/ErrorFallback';
 
 export const OtherPagesLayout = () => {
   return (
-    <ErrorBoundary FallbackComponent={ErrorFallback}>
-      <div className="w-full overflow-y-auto">
-        <Outlet />
-      </div>
-    </ErrorBoundary>
+    <div className="w-full overflow-y-auto">
+      <Outlet />
+    </div>
   );
 };
