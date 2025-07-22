a8a4a903f7626969e7624c1e514fd616057f037a
allison-truhlar <truhlara@janelia.hhmi.org>
2025-07-22 10:12:51 -0400

    test commit

--- src/components/Help.tsx
+++ src/components/Help.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function Help() {
+export default function Help( ) {
   return (
     <div className="p-4">
       <h2 className="text-foreground text-lg">Help Page</h2>
