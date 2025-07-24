9bdd6c9a39b9f2cd6b397b24d432303be0f26daf
allison-truhlar <truhlara@janelia.hhmi.org>
2025-07-24 10:12:04 -0400

    feat: example implementation of new error handling pattern in a hook
    - Added try/catch block inside handleFavoriteToggleMenuItemClick, which is used in the UI
    - This function now returns a Result, either a Success or Failure
    - The error is logged in the catch before the Failure is returned, to ensure the developers can see to the full error but the user only sees a simplified string

--- src/hooks/useContextMenu.ts
+++ src/hooks/useContextMenu.ts
@@ -1,8 +1,14 @@
 import * as React from 'react';
+import logger from '@/logger';
 
-import { FileOrFolder } from '@/shared.types';
+import type { FileOrFolder, Result } from '@/shared.types';
 import { useFileBrowserContext } from '@/contexts/FileBrowserContext';
 import { usePreferencesContext } from '@/contexts/PreferencesContext';
+import {
+  createSuccessResult,
+  createFailureResult,
+  parseError
+} from '@/utils/errorHandling';
 
 export default function useContextMenu() {
   const [contextMenuCoords, setContextMenuCoords] = React.useState({
@@ -73,18 +79,24 @@ export default function useContextMenu() {
 
   const handleFavoriteToggleMenuItemClick = async (
     selectedFiles: FileOrFolder[]
-  ): Promise<void> => {
-    if (currentFileSharePath) {
-      await handleFavoriteChange(
-        {
-          type: 'folder',
-          folderPath: selectedFiles[0].path,
-          fsp: currentFileSharePath
-        },
-        'folder'
-      );
+  ): Promise<Result<null>> => {
+    try {
+      if (currentFileSharePath) {
+        await handleFavoriteChange(
+          {
+            type: 'folder',
+            folderPath: selectedFiles[0].path,
+            fsp: currentFileSharePath
+          },
+          'folder'
+        );
+        setShowContextMenu(false);
+      }
+    } catch (error) {
+      logger.error(error);
+      return createFailureResult(parseError(error));
     }
-    setShowContextMenu(false);
+    return createSuccessResult();
   };
 
   return {
