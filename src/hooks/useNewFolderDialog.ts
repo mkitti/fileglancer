ff69eba0da4f87684b92a5060657c18a5859ed17
allison-truhlar <truhlara@janelia.hhmi.org>
2025-07-29 15:00:10 -0400

    refactor: change NewFolder to use new err handling

--- src/hooks/useNewFolderDialog.ts
+++ src/hooks/useNewFolderDialog.ts
@@ -1,82 +1,55 @@
-import { useState } from 'react';
-import toast from 'react-hot-toast';
+import React from 'react';
 
+import { getFileBrowsePath, sendFetchRequest, joinPaths } from '@/utils';
 import {
-  getFileBrowsePath,
-  sendFetchRequest,
-  joinPaths,
-  getPreferredPathForDisplay
-} from '@/utils';
+  createSuccess,
+  handleBadResponse,
+  handleError
+} from '@/utils/errorHandling';
 import { useFileBrowserContext } from '@/contexts/FileBrowserContext';
 import { useCookiesContext } from '@/contexts/CookiesContext';
-import { usePreferencesContext } from '@/contexts/PreferencesContext';
+import type { Result } from '@/shared.types';
 
 export default function useNewFolderDialog() {
-  const [newName, setNewName] = useState<string>('');
-  const [showAlert, setShowAlert] = useState<boolean>(false);
-  const [alertContent, setAlertContent] = useState<string>('');
+  const [newName, setNewName] = React.useState<string>('');
 
   const { currentFolder, refreshFiles } = useFileBrowserContext();
   const { currentFileSharePath } = useFileBrowserContext();
-  const { pathPreference } = usePreferencesContext();
   const { cookies } = useCookiesContext();
 
-  async function addNewFolder() {
+  async function handleNewFolderSubmit(): Promise<Result<void>> {
     if (!currentFileSharePath) {
-      throw new Error('No file share path selected.');
+      return handleError(new Error('No file share path selected.'));
     }
     if (!currentFolder) {
-      throw new Error('No current file or folder selected.');
+      return handleError(new Error('No current file or folder selected.'));
     }
-    await sendFetchRequest(
-      getFileBrowsePath(
-        currentFileSharePath.name,
-        joinPaths(currentFolder.path, newName)
-      ),
-      'POST',
-      cookies['_xsrf'],
-      {
-        type: 'directory'
-      }
-    );
-  }
-
-  async function handleNewFolderSubmit() {
-    setShowAlert(false);
-    if (!currentFileSharePath) {
-      setAlertContent('No file share path selected.');
-      return false;
-    } else if (!currentFolder) {
-      setAlertContent('No current file or folder selected.');
-      return false;
-    } else {
-      const displayPath = getPreferredPathForDisplay(
-        pathPreference,
-        currentFileSharePath,
-        `${currentFolder.path}/${newName}`
+    try {
+      const response = await sendFetchRequest(
+        getFileBrowsePath(
+          currentFileSharePath.name,
+          joinPaths(currentFolder.path, newName)
+        ),
+        'POST',
+        cookies['_xsrf'],
+        {
+          type: 'directory'
+        }
       );
-      try {
-        await addNewFolder();
+      if (response.ok) {
         await refreshFiles();
-        const alertContent = `Created new folder at path: ${displayPath}`;
-        toast.success(alertContent);
-        return true;
-      } catch (error) {
-        const errorContent = `Error creating new folder at path: ${displayPath}`;
-        setAlertContent(
-          `${errorContent}. Error details: ${error instanceof Error ? error.message : 'Unknown error'}`
-        );
-        return false;
+      } else {
+        return handleBadResponse(response);
       }
+    } catch (error) {
+      return handleError(error);
     }
+    return createSuccess();
   }
 
   return {
     handleNewFolderSubmit,
     newName,
-    setNewName,
-    showAlert,
-    setShowAlert,
-    alertContent
+    setNewName
   };
 }
