9c13dd0305f058c3d8478316eda353aa422627ed
allison-truhlar <truhlara@janelia.hhmi.org>
2025-08-05 09:40:20 -0400

    refactor: add a beforeEach block to render Sidebar in each test

--- src/__tests__/componentTests/Sidebar.test.tsx
+++ src/__tests__/componentTests/Sidebar.test.tsx
@@ -1,21 +1,26 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, beforeEach } from 'vitest';
 import userEvent from '@testing-library/user-event';
 
 import Sidebar from '@/components/ui/Sidebar/Sidebar';
-import { render, screen } from '@/__tests__/test-utils';
+import { render, screen, waitFor } from '@/__tests__/test-utils';
 
 describe('Sidebar', () => {
+  beforeEach(async () => {
+    render(<Sidebar />, { initialEntries: ['/browse'] });
+    await waitFor(() => {
+      expect(screen.getByRole('searchbox')).toBeInTheDocument();
+    });
+  });
+
   it('displays all zones initially', async () => {
-    render(<Sidebar />);
-    expect(await screen.findByText('Zone1')).toBeInTheDocument();
-    expect(await screen.findByText('Zone2')).toBeInTheDocument();
+    expect(screen.getByText('Zone1')).toBeInTheDocument();
+    expect(screen.getByText('Zone2')).toBeInTheDocument();
   });
 
   it('filters zones', async () => {
     const user = userEvent.setup();
-    render(<Sidebar />);
 
-    await user.click(await screen.findByRole('searchbox'));
+    await user.click(screen.getByRole('searchbox'));
     await user.keyboard('1');
 
     expect(screen.getByText('Zone1')).toBeInTheDocument();
