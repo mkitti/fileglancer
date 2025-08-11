689a3c998f21a10ab88dcee545c81860ef413bf3
allison-truhlar <truhlara@janelia.hhmi.org>
2025-08-11 14:25:13 -0400

    refactor: make handleError not async; add func to handle http errs

--- src/utils/errorHandling.ts
+++ src/utils/errorHandling.ts
@@ -5,32 +5,31 @@ function createSuccess<T>(data: T): Success<T> {
   return { success: true, data };
 }
 
-async function getResponseError(response: Response): Promise<string> {
+async function toHttpError(response: Response): Promise<Error> {
   const body = await response.json();
-  return `${response.status}: ${body.error ? body.error : 'Unknown error'}`;
+  return new Error(
+    `${response.status}: ${body.error ? body.error : 'Unknown error'}`
+  );
 }
 
 function createFailure(error: string): Failure {
   return { success: false, error };
 }
 
-async function getErrorString(error: unknown): Promise<string> {
+function getErrorString(error: unknown): string {
   if (typeof error === 'string') {
     return error;
   }
   if (error instanceof Error) {
     return error.message;
   }
-  if (error instanceof Response) {
-    return await getResponseError(error);
-  }
   return 'An unknown error occurred';
 }
 
-async function handleError(error: unknown): Promise<Failure> {
-  const errorString = await getErrorString(error);
+function handleError(error: unknown): Failure {
+  const errorString = getErrorString(error);
   logger.error(errorString);
   return createFailure(errorString);
 }
 
-export { createSuccess, handleError, getResponseError };
+export { createSuccess, handleError, toHttpError };
